4f7a434aef6d1aabef62388e3992372eeb5a0385
jTec <kietpawpan@gmail.com>
2022-04-20 18:48:52 +0700

    Update infoXinstaller.bat

--- infoXinstaller.bat
+++ infoXinstaller.bat
@@ -2,6 +2,6 @@
 ::This part will unzip files
 
 echo "Unziping file ...."
-powershell -Command "Expand-Archive infox-1.2.8.zip -DestinationPath C:\infoX"
+powershell -Command "Expand-Archive infox-1.2.9.zip -DestinationPath C:\infoX"
 echo "Done! Please go to the folder infoX in Drive C:, and double click the infoX file."
 pause
