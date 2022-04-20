a666ff99077cc924cc404bf4ece376e9d12598f1
jTec <kietpawpan@gmail.com>
2022-04-21 01:15:43 +0700

    Delete infoXinstaller.bat

--- infoXinstaller.bat
+++
@@ -1,7 +0,0 @@
-@ echo off
-::This part will unzip files
-
-echo "Unziping file ...."
-powershell -Command "Expand-Archive infox-1.3.0.zip -DestinationPath C:\infoX"
-echo "Done! Please go to the folder infoX in Drive C:, and double click the infoX file."
-pause
