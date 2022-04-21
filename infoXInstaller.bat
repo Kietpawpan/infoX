fe11f5b7f0551ad8bc0ce734924fc96f94c29e2c
jTec <kietpawpan@gmail.com>
2022-04-21 23:25:02 +0700

    Update infoXInstaller.bat

--- infoXInstaller.bat
+++ infoXInstaller.bat
@@ -2,9 +2,9 @@
 ::This part will unzip files
 
 echo "Installing file . . . ."
-powershell -Command "Expand-Archive infox-1.3.2.zip -DestinationPath C:\infoX"
-powershell $TargetFile = "C:/infoX/infoX-1.3.2/infoX.hta"
+powershell -Command "Expand-Archive infox-1.3.3.zip -DestinationPath C:\infoX"
+powershell $TargetFile = "C:/infoX/infoX-1.3.3/infoX.hta"
 powershell $ScriptShell = New-Object -ComObject WScript.Shell
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\infoX.lnk');$s.TargetPath='C:\infoX\infoX-1.3.2\infoX.hta';$s.IconLocation='C:\infoX\infoX-1.3.2\Dtrees\image\MNRE.ico';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\infoX.lnk');$s.TargetPath='C:\infoX\infoX-1.3.3\infoX.hta';$s.IconLocation='C:\infoX\infoX-1.3.3\Dtrees\image\MNRE.ico';$s.Save()"
 echo "Done!"
 pause
