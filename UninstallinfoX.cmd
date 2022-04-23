4b3e89e40d70381d9ea42323c124062d2cb01ccc
jTec <kietpawpan@gmail.com>
2022-04-24 02:36:22 +0700

    Update UninstallinfoX.cmd

--- UninstallinfoX.cmd
+++ UninstallinfoX.cmd
@@ -3,5 +3,5 @@ echo "Unstalling . . ."
 IF EXIST "C:\infoX" (
     rmdir "C:\infoX" /s /q
 )
-echo "infoX DSS (Version 1.3.4) was successfully uninstalled."
+echo "infoX DSS was successfully uninstalled."
 pause
